adca1c5d1f4cfc44f29ed741fd74cddaffba0bdc
trymad <trymad@gmail.com>
2025-04-23 17:12:22 +0300

    change archieve table

--- src/main/java/com/trymad/hahaton/HahatonApplication.java
+++ src/main/java/com/trymad/hahaton/HahatonApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HahatonApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {g
 		SpringApplication.run(HahatonApplication.class, args);
 	}
 
